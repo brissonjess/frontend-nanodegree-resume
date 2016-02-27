32982042e7bbb4804152791c6d1e19f3310b086a
Jessica Brisson <brissonjess@gmail.com>
2016-02-27 16:10:58 -0700

    Add education object that outlines my education background

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,42 +1,67 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- var name = "Jessica Brisson";
- var role = "Web Developer, Designer";
- var formattedName = HTMLheaderName.replace("%data%",name);
- var formattedRole = HTMLheaderRole.replace("%data%",role);
- var skills = ["CSS3","HTML5","JavaScript","Digital Design"];
- var bio = {
- 	"skills": skills,
- 	"name": "Jessica",
- 	"contact": {
- 		"phone": "780-885-4151",
- 		"twitter": "@brissonjess",
- 		"email": "brissonjess@gmail.com",
- 		"location": "Edmonton, AB"
- 	},
- 	"role" : "Web Designer, Developer",
- 	"pic": "images/fry.jpg"
- };
- bio.education = {
- 	"macewanName": "Grant MacEwan University",
- 	"macewanYears": "2 years",
- 	"macewanDiploma": "Journalism Diploma"
- };
+var name = "Jessica Brisson";
+var role = "Web Developer, Designer";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+var skills = ["CSS3","HTML5","JavaScript","Digital Design"];
+var bio = {
+	"skills": skills,
+	"name": "Jessica",
+	"contact": {
+		"phone": "780-885-4151",
+		"twitter": "@brissonjess",
+		"email": "brissonjess@gmail.com",
+		"location": "Edmonton, AB"
+	},
+	"role" : "Web Designer, Developer",
+	"pic": "images/fry.jpg"
+};
+var education = {
+	"schools": [{
+		"name": "Grant MacEwan University",
+		"city": "Edmonton, AB",
+		"degree": "Associate Degree",
+		"major": ["Journalism"],
+		"minor": "NA",
+		"graduationYears": "2008-2010"
+	}, {
+		"name": "Northern Alberta Institue of Technology",
+		"city": "Edmonton, AB",
+		"degree": "Associate Degree",
+		"major": "Computer Software Development",
+		"minor": "NA",
+		"graduationYears": "2014-Present"
+	}],
+	"onlineCourses": [{
+		"title": "JavaScript Syntax",
+		"school": "Udacity",
+		"date": "2016",
+		"url": "http://www.udacity.com/course/ud804"
+	}, {
+		"title": "Linux Command Line Basics",
+		"school": "Udacity",
+		"date": "2016",
+		"url": "http://www.udacity.com/course/ud595"
+	}, {
+		"title": "How to Use Git and GitHub",
+		"school": "Udacity",
+		"date": "2016",
+		"url": "http://www.udacity.com/course/ud775"
+	}]
+}
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#header").append(bio.contact.phone,
-					bio.contact.email,
-					bio.contact.twitter,
-					bio.contact.location);
+				bio.contact.email,
+				bio.contact.twitter,
+				bio.contact.location);
 $("#header").append(bio.skills);
 $("#main").append(bio.pic);
 $("#main").append(
-	bio.education.macewanName,
-	bio.education.macewanYears,
-	bio.education.macewanDiploma
-	);
+bio.education.macewanName,
+bio.education.macewanYears,
+bio.education.macewanDiploma
+);
 
 
 //name
